9af50bebbf9e4731a2f56bc4dec4511007703b5d
ywh123456789 <1945609662@qq.com>
2025-03-28 15:22:31 +0800

    Site updated: 2025-03-28 15:22:13

--- js/cat.js
+++ js/cat.js
@@ -34,8 +34,8 @@ if (document.body.clientWidth > 992) {
                 borderRadius: 5 + 'px',
                 right: 60 + 'px',
                 // 这里可以换为你喜欢的图片，例如我就换为了雪人，但是要抠图
-                nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
-                hoverMsg: "喵喵喵~",
+                nekoImg: "/img/siyecao.png",
+                hoverMsg: "Lucky!",
                 color: "#6f42c1",
                 during: 500,
                 blog_body: "body",
